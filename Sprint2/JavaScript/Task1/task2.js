be59df6bef3d5c7a939cc8de15a39f0098bc69cb
J S Lekshmi Priya <lekshmipriya8212@gmail.com>
2025-02-20 15:14:06 +0530

    Add files via upload

---
+++ Sprint2/JavaScript/Task1/task2.js
@@ -0,0 +1,99 @@
+//1//
+let firstName="Lekshmi Priya"
+let under_score ="20" 
+let $case ="Yeww";
+console.log(firstName)
+console.log(under_score)
+console.log($case)
+//2//
+const num=10;
+//3//
+let array=["Apple","Mango","Cherry","Berry"];
+console.log(array[1]);
+console.log(array[2]);
+//4//
+let add = 5+5;
+console.log(add)
+//5//
+let radius=5;
+let Area=Math.PI *radius *radius;
+console.log("Area of the circle is:",Area.toFixed(2));
+//6//
+let length=10;
+let width=5;
+let area= length*width;
+console.log("Area of a rectangle is :",area)
+//7//
+let base=2; let height=4;
+let area1=(base*height)/2;
+console.log("Area of a triangle is:",area1)
+//8//
+let u=10,v=5;
+console.log("Addition:",(u+v))
+console.log("Division:",(u/v))
+console.log("Multiplication:",(u*v))
+console.log("Subtraction:",(u-v))
+//9//
+let a=20;
+console.log(a)
+a+=10;
+console.log("After +:",a)
+a-=5
+console.log("After -:",a)
+a*=2;
+console.log("After *:",a)
+a**=3;
+console.log("After **:",a)
+a/=10;
+console.log("After /:",a)
+
+//10//
+let val = 10;
+val++;  
+console.log("After Increment:", val); 
+val--;  
+console.log("After Decrement:", val); 
+
+// 11
+let m = 7;
+let n = "7";
+
+console.log("m == n:", m == n);   
+console.log("m === n:", m === n); 
+console.log("m != n:", m != n);   
+console.log("m !== n:", m !== n); 
+console.log("m > n:", m > n);     
+console.log("m < n:", m < n);     
+console.log("m >= n:", m >= n);   
+console.log("m <= n:", m <= n); 
+
+// 12
+let _true = true;
+let _false = false;
+console.log("AND (&&):", _true && _false);
+console.log("OR (||):", _true || _false);
+console.log("NOT (!):", !_true);
+
+// 13
+let p = 5, q = 10;
+[p, q] = [q, p];
+console.log('After Swap: p = {p}, q = {q}');
+
+// 14
+let num_1 = 10, num_2 = 20, num_3 = 30;
+let average = (num_1 + num_2 + num_3) / 3;
+console.log("Average:", average);
+
+// 15
+let _a = 10, _b = 30, _c = 12, _d = 3;
+let result = (_a + _b) * _c / _d;
+console.log("Final Result:", result);
+
+// 16
+let tamil = 90, english = 90, maths = 90, science = 90, social = 90;
+
+let totalMarks = tamil + english + maths + science + social;
+let averageMarks = totalMarks / 5;
+console.log("Total Marks:", totalMarks);
+console.log("Average Marks:", averageMarks);
+
